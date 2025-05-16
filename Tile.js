8d39dd8cd499393e6b2f6c6771bc426783b57eeb
shafiaakbar <shafia.akbar1@gmail.com>
2025-05-16 16:30:27 +0500

    init

--- Tile.js
+++ Tile.js
@@ -1,53 +1,46 @@
-class Tile{
-
-  constructor(x,y){
-    this.matrixPos = createVector(x,y);
-    this.pixelPos = createVector(x*tileSize+xoff, y*tileSize+yoff);
-    this.safe = false;
-    this.goal = false;
-    this.wall = false;
-    this.edges = [];
-
+class Tile {
+  constructor(x, y) {
+    this.matrixPos = createVector(x, y)
+    this.pixelPos = createVector(x * tileSize + xoff, y * tileSize + yoff)
+    this.safe = false
+    this.goal = false
+    this.wall = false
+    this.edges = []
   }
 
-   show(){
-     if ((this.matrixPos.x + this.matrixPos.y) % 2 ==0) {
-      fill(230,230,255);
-    } else {
-      fill(230,230,255);
-    }
+  show() {
+    // Draw background tile
     if (this.wall) {
-      fill(230,230,255);
+      fill(0) // Black walls like Pacman maze
+    } else if (this.safe || this.goal) {
+      fill('#FFD700') // Glowing gold for goal/safe zones
+    } else {
+      fill('#E8E3FF') // Light purple paths
     }
-    if (this.goal || this.safe) {
-      fill(0, 0, 0);
-    }
-    noStroke();
-    rect(this.pixelPos.x,this.pixelPos.y,tileSize,tileSize);
 
+    stroke(50) // Subtle grid lines
+    strokeWeight(1)
+    rect(this.pixelPos.x, this.pixelPos.y, tileSize, tileSize)
   }
 
-  showEdges(){
-    for (var i = 0; i< this.edges.length; i++) {
-      stroke(0);
-      strokeWeight(4);
-      switch(this.edges[i]) {
-        case 4:
-          line(this.pixelPos.x, this.pixelPos.y, this.pixelPos.x+tileSize,this.pixelPos.y);
-          break;
-        case 1:
-          line(this.pixelPos.x+tileSize, this.pixelPos.y, this.pixelPos.x+tileSize, this.pixelPos.y+tileSize);
-          break;
-        case 2:
-          line(this.pixelPos.x, this.pixelPos.y+tileSize, this.pixelPos.x+tileSize, this.pixelPos.y+tileSize);
-          break;
-        case 3:
-          line(this.pixelPos.x, this.pixelPos.y, this.pixelPos.x, this.pixelPos.y+tileSize);
-          break;
+  showEdges() {
+    for (let i = 0; i < this.edges.length; i++) {
+      stroke(0)
+      strokeWeight(4)
+      switch (this.edges[i]) {
+        case 1: // Right
+          line(this.pixelPos.x + tileSize, this.pixelPos.y, this.pixelPos.x + tileSize, this.pixelPos.y + tileSize)
+          break
+        case 2: // Bottom
+          line(this.pixelPos.x, this.pixelPos.y + tileSize, this.pixelPos.x + tileSize, this.pixelPos.y + tileSize)
+          break
+        case 3: // Left
+          line(this.pixelPos.x, this.pixelPos.y, this.pixelPos.x, this.pixelPos.y + tileSize)
+          break
+        case 4: // Top
+          line(this.pixelPos.x, this.pixelPos.y, this.pixelPos.x + tileSize, this.pixelPos.y)
+          break
       }
     }
   }
-
-
-
 }
